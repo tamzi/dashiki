846ffa8f567f562d05e049553de6bafb46d88eb9
tamzi <tamrefrank@gmail.com>
2022-08-25 21:15:29 +0300

    Update to project:Fails on build for now(DS fix

---
+++ dashikiDesignSystem/src/main/java/com/tamzi/dashikidesignsystem/atoms/Color.kt
@@ -0,0 +1,43 @@
+package com.tamzi.dashikidesignsystem.atoms
+
+
+import androidx.compose.ui.graphics.Color
+
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF131313)
+
+/*Coffee  Isnpired*/
+val kahawaBrown = Color(0xFFB78358)
+val kahawaDeepBrown = Color(0xFF541F08)
+val kahawaLightBrown = Color(0xFFAF620E)
+val kahawaSmoke = Color(0xFFD6B59C)
+val kahawaCream = Color(0xFFDDAA78)
+val kahawaDarkGrey = Color(0xFF353439)
+val kahawaBlack = Color(0xFF1B1115)
+
+
+val kahawaRed = Color(0xFFFF5000)
+val reddish = Color(0xFFFF4343)
+val warmRed = Color(0xFFFF7754)
+
+
+val paragraphTextColor = Color(0xFFffbd4a)
+
+
+val kahawaGreen = Color(0xFF006643)
+val lime = Color(0xFF55B03E)
+val limeGreenish = Color(0xFF4ABB00)
+val teal= Color(0xFF0d9668)
+val bgColor = Color(0xFF91b5a3)
+
+
+val bluish = Color(0xFF5944BE)
+val darkBluish = Color(0xFF312651)
+
+
+
+val gray = Color(0xFFEFEFEF)
+val deepGray = Color(0xFFE6E4E6)
+val darkGray = Color( 0xFF83829A)
+val lightGray = Color(0xFFF6F6F6)
+val buttonRed = Color(0xFF7777CC)
