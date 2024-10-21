dd4c394704b7c1b08ae21a2ce9e05780aae09596
./root <tamrefrank@gmail.com>
2024-10-22 00:17:05 +0200

    upgrading

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,5 +9,5 @@ plugins {
     id ("org.jetbrains.kotlin.android") version ("1.8.21") apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.21" apply false
     id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
-    id("com.android.test") version "8.0.2" apply false
+    id("com.android.test") version "8.7.1" apply false
 }
